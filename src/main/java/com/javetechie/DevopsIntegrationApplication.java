74e3e758142227ccdc7955943e785a91049d2301
Adnane1998 <mbarki.adnane98@gmail.com>
2023-06-05 21:33:03 +0200

    sd

---
+++ src/main/java/com/javetechie/DevopsIntegrationApplication.java
@@ -0,0 +1,20 @@
+package com.javetechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsIntegrationApplication {
+	@GetMapping
+	public String message(){
+		return "welcome to javatechie";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
+}
